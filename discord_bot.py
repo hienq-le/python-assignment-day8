a9422a934f61debf56306be80ab7f7a42312e2dd
hienq-le <124135405+hienq-le@users.noreply.github.com>
2023-03-13 20:51:03 +0700

    Add files via upload
    Updated the code because the py-cord package has been updated

--- discord_bot.py
+++ discord_bot.py
@@ -15,14 +15,15 @@ intents6 = discord.Intents().all()
 client6 = discord.Client(intents=intents6)
 handler7 = logging.FileHandler(filename='discord_bot.log', encoding='utf-8', mode='w')
 bot8_1 = discord.Bot(intents=intents6)
-channel_id = bot8_1.get_channel(1070965420137381888)
+client_id = int(os.environ.get('CLIENT_ID'))
+channel_id = int(os.environ.get('CHANNEL_ID'))
 # Log in
 @bot8_1.event
 async def on_ready():
 	print(str(bot8_1.user)+' successfully logged in!')
 # Look for a cat image in the device, send a cat image, then wait 5 minutes and repeat
-@bot8_1.slash_command(name="sendcatimageevery5minutes", description="Send a cat image every 5 minutes")
-async def send_cat_image_every_5_minutes(self):
+@bot8_1.command(description="Send a cat image every 5 minutes")
+async def sendcatimageevery5minutes(ctx):
 	i80 = 0
 	while i80 == 0:
 		i80 = 0
@@ -30,22 +31,22 @@ async def send_cat_image_every_5_minutes(self):
 			file_to_send = open('20230203_062142_cat.jpg', 'r')
 			file_to_send.close()
 		except FileNotFoundError:
-			await self.respond('Fail to send a cat image!', ephemeral=True)
+			await bot8_1.get_channel(channel_id).send('Fail to send a cat image!')
 			print('Fail to send a cat image!')
 			time.sleep(300)
 			continue
-		await self.respond('Sent a cat image\n', file=discord.File('20230203_062142_cat.jpg'), ephemeral=True)
+		await bot8_1.get_channel(channel_id).send('Sent a cat image\n', file=discord.File('20230203_062142_cat.jpg'))
 		print('successfully sent a cat image!')
 		time.sleep(300)
 		continue
 # Search cat images with '/searchimage cat' command
-@bot8_1.slash_command(name="searchimage", description="Search images with a certain keyword")
-async def foo555(self, keyword):
+@bot8_1.command(description="Search images with a certain keyword")
+async def searchimage(ctx, keyword):
 	try:
 		if keyword == None or keyword == '\0':
 			raise ValueError
 	except ValueError:
-		await self.respond('Keyword required', ephemeral=True)
+		await bot8_1.get_channel(channel_id).send('Keyword required')
 		print("Keyword required!")
 		return None
 	random555 = random.randint(0, 9)
@@ -54,7 +55,7 @@ async def foo555(self, keyword):
 	url555 = result555["items"][random555]["link"]
 	embed555 = discord.Embed(title="Here\'s your "+keyword+" image:\n")
 	embed555.set_image(url=url555)
-	await self.respond(embed=embed555, ephemeral=True)
+	await bot8_1.get_channel(channel_id).send(embed=embed555)
 try:
 	bot8_1.run(os.environ.get('DISCORD_TOKEN'))
 except discord.errors.LoginFailure:
